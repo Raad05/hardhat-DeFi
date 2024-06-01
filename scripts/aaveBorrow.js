d3bfcb8ad62c355bcf583e9ad4d7431aef7d99df
Raad05 <ashabulraad@gmail.com>
2024-06-01 21:27:57 +0600

    retrieved lending pool address

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,4 +1,4 @@
-const { getNamedAccounts } = require("hardhat");
+const { getNamedAccounts, ethers } = require("hardhat");
 const { getWeth } = require("./getWeth");
 
 const main = async () => {
@@ -6,7 +6,25 @@ const main = async () => {
 
   const { deployer } = await getNamedAccounts();
 
+  const signer = await ethers.provider.getSigner();
+
   // Lending pool address provider v3: 0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e
+  const pool = await getPool(signer);
+  console.log("Pool address:", pool.target);
+};
+
+const getPool = async (account) => {
+  const poolAddressesProvider = await ethers.getContractAt(
+    "IPoolAddressesProvider",
+    "0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e",
+    account
+  );
+
+  const poolAddress = await poolAddressesProvider.getPool();
+
+  const pool = await ethers.getContractAt("IPool", poolAddress, account);
+
+  return pool;
 };
 
 main()
