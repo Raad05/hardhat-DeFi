471163d7e7fbf5656379b5802978fecf8af8395d
Raad05 <ashabulraad@gmail.com>
2024-06-01 18:43:18 +0600

    added PoolAddressProvider interface

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,7 +7,11 @@ const { ALCHEMY_FORKED_MAINNET } = process.env;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    compilers: [{ version: "0.8.24" }, { version: "0.4.19" }],
+    compilers: [
+      { version: "0.8.24" },
+      { version: "0.4.19" },
+      { version: "0.8.0" },
+    ],
   },
   networks: {
     hardhat: {
--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,7 +1,12 @@
+const { getNamedAccounts } = require("hardhat");
 const { getWeth } = require("./getWeth");
 
 const main = async () => {
   await getWeth();
+
+  const { deployer } = await getNamedAccounts();
+
+  // Lending pool address provider v3: 0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e
 };
 
 main()
